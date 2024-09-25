ef2cba362fab1f7fe8ee10c638763aa1cc0ffb6c
Lilla Németh <naemly@protonmail.com>
2024-09-25 18:29:27 +0300

    updating Dashboard as it needs hooks -> putting data fetching into useEffect and store data in hooks (users, error message); search hook and onChange on input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+'use client';
+import { ChangeEvent, useEffect, useState } from 'react';
 import styles from '@/app/styles/Dashboard.module.scss';
+
+// Components
 import Card from './components/Card';
 import Input from './components/Input';
 import Button from './components/Button';
+
+// Helpers
+import { handleInputChange } from './utils/helpers';
 
 interface UserLocation {
 	lat: string;
@@ -25,20 +32,28 @@ interface User {
 	address: UserAddress[];
 }
 
-export default async function Dashboard() {
-	let users: User[] = [];
-	let error: string | undefined;
+export default function Dashboard() {
+	const [search, setSearch] = useState('');
+	const [users, setUsers] = useState<User[]>([]);
+	const [error, setError] = useState<string | undefined>('');
 
-	try {
-		const res = await fetch('http://localhost:3000/api/users');
-		if (!res.ok) {
-			// when failed to fetch data, user friendly error message is displayed:
-			throw new Error('Something went wrong, please come back later.');
-		}
-		users = await res.json();
-	} catch (err) {
-		error = (err as Error).message;
-	}
+	useEffect(() => {
+		const fetchUsers = async () => {
+			try {
+				const res = await fetch('http://localhost:3000/api/users');
+				if (!res.ok) {
+					// when failed to fetch data, user friendly error message is displayed:
+					throw new Error('Something went wrong, please come back later.');
+				}
+				const userData: User[] = await res.json();
+				setUsers(userData);
+			} catch (err) {
+				setError((err as Error).message);
+			}
+		};
+
+		fetchUsers();
+	}, []);
 
 	if (error) {
 		return <div className='errorCard'>{error}</div>;
@@ -48,7 +63,14 @@ export default async function Dashboard() {
 		<div>
 			<div style={{ fontSize: '40px', display: 'flex', padding: '10px', justifyContent: 'center' }}>Users</div>
 			<div style={{ display: 'flex', padding: '40px', justifyContent: 'center' }}>
-				<Input className={styles.searchInput} htmlFor={'card-search'} type={'search'} name={'search'} />
+				<Input
+					className={styles.searchInput}
+					htmlFor={'card-search'}
+					type={'search'}
+					name={'search'}
+					value={search}
+					onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(setSearch, e)}
+				/>
 				<Button type={'submit'} text={'Search'} />
 			</div>
 			<div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
