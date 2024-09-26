d92990c1855dd1e8491acb544176162fa205443c
Lilla Németh <naemly@protonmail.com>
2024-09-26 11:52:10 +0300

    deleting ToggleButton as Button component can be used instead; creating DropdownList for selecting sort category

---
+++ src/app/components/DropdownList.tsx
@@ -0,0 +1,9 @@
+function DropdownList() {
+	return (
+		<div>
+			<div></div>
+		</div>
+	);
+}
+
+export default DropdownList;
--- src/app/components/ToggleButton.tsx
+++
@@ -1,9 +0,0 @@
-function ToggleButton() {
-	return (
-		<div>
-			<div></div>
-		</div>
-	);
-}
-
-export default ToggleButton;
