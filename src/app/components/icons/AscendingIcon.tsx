4696861d4607db228e9161b2bb037b9da0ff860c
Lilla Németh <naemly@protonmail.com>
2024-09-26 17:41:12 +0300

    fixing AscendingIcon to make it react compatible

--- src/app/components/icons/AscendingIcon.tsx
+++ src/app/components/icons/AscendingIcon.tsx
@@ -4,16 +4,16 @@ function AscendingIcon(props: any) {
 	return (
 		<svg
 			className={className}
-			clip-rule='evenodd'
-			fill-rule='evenodd'
-			stroke-linejoin='round'
-			stroke-miterlimit='2'
+			clipRule='evenodd'
+			fillRule='evenodd'
+			strokeLinejoin='round'
+			strokeMiterlimit='2'
 			viewBox='0 0 24 24'
 			xmlns='http://www.w3.org/2000/svg'
 		>
 			<path
 				d='m8.656 5.778c0 .414.336.75.75.75h5.16c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-5.16c-.414 0-.75.336-.75.75zm-2.206 4c0 .414.336.75.75.75h9.596c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-9.596c-.414 0-.75.336-.75.75zm-2.45 4c0 .414.336.75.75.75h14.5c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-14.5c-.414 0-.75.336-.75.75zm-2 4c0 .414.336.75.75.75h18.5c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75z'
-				fill-rule='nonzero'
+				fillRule='nonzero'
 			/>
 		</svg>
 	);
