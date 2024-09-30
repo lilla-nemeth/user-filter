7340dfe2c30a40547c7008e07b1c61c9edc0376b
Lilla Németh <naemly@protonmail.com>
2024-09-30 22:19:06 +0300

    test for handleDefaultName

--- src/app/__tests__/helperFunctions.test.ts
+++ src/app/__tests__/helperFunctions.test.ts
@@ -1,5 +1,12 @@
 import { ChangeEvent, Dispatch, SetStateAction } from 'react';
-import { capitalizeString, handleButtonClick, handleDisplay, handleText, handleUserData } from '../utils/helperFunctions';
+import {
+	capitalizeString,
+	handleButtonClick,
+	handleDefaultName,
+	handleDisplay,
+	handleText,
+	handleUserData,
+} from '../utils/helperFunctions';
 import { User } from '@/types/data';
 import { mockApiUsers, mockFilteredUsers } from '../__mocks__/testUsers';
 
@@ -62,5 +69,37 @@ describe('handleDisplay', () => {
 
 		handleDisplay(initialOpenState, setStateMock);
 		expect(setStateMock).toHaveBeenCalledWith(false);
+	});
+});
+
+describe('handleDefaultName', () => {
+	it('should call stateSetter when isOpen false and category name does not match with the string', () => {
+		const setStateMock: Dispatch<SetStateAction<string>> = jest.fn();
+		const isOpen = false;
+		const categoryName = 'Name';
+		const str = 'Sort by';
+
+		handleDefaultName(isOpen, categoryName, str, setStateMock);
+		expect(setStateMock).toHaveBeenCalledWith(str);
+	});
+
+	it('should not call stateSetter when isOpen is true (string matching is irrelevant here)', () => {
+		const setStateMock: Dispatch<SetStateAction<string>> = jest.fn();
+		const isOpen = true;
+		const categoryName = 'Email';
+		const str = 'Sort By';
+
+		handleDefaultName(isOpen, categoryName, str, setStateMock);
+		expect(setStateMock).not.toHaveBeenCalled();
+	});
+
+	it('should not call stateSetter when isOpen false and categoryName matches with the string', () => {
+		const setStateMock: Dispatch<SetStateAction<string>> = jest.fn();
+		const isOpen = false;
+		const categoryName = 'Email';
+		const str = 'Email';
+
+		handleDefaultName(isOpen, categoryName, str, setStateMock);
+		expect(setStateMock).not.toHaveBeenCalled();
 	});
 });
